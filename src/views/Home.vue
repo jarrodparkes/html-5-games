742a4cb3d56dd2d192f2f8c6075f4e0ce83e8f09
Jarrod Parkes <parkesfjarrod@gmail.com>
2019-11-11 09:20:35 -0600

    add logo to index and include build in travis run

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <ul>
-    <li><router-link :to="{ name: 'dodge' }">Dodge</router-link></li>
-    <li><router-link :to="{ name: 'snake' }">Snake</router-link></li>
-  </ul>
+  <div>
+    <img src="../assets/logo.png" />
+    <ul>
+      <li><router-link :to="{ name: 'dodge' }">Dodge</router-link></li>
+      <li><router-link :to="{ name: 'snake' }">Snake</router-link></li>
+    </ul>
+  </div>
 </template>
 
 <script>
